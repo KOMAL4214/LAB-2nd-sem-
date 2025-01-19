dda08d90eedcd61d822f1e4ac3f8e8320a5f91fd
KOMAL4214 <jksai2006@gmail.com>
2025-01-19 12:56:31 +0530

    Update and rename integer squares 1 to 50.py to 1.1(a).py

---
+++ 1.1(a).py
@@ -0,0 +1,5 @@
+print("program to get a list of squares of 1 to 50")
+list=[]
+for i in range (0,50) :
+    list.append(i)
+print(list)
--- integer squares 1 to 50.py
+++
@@ -1,6 +0,0 @@
-print("program to get a list of 'a' to 'zzz...z'")
-list=[]
-x=1
-for i in range (0,26) :
-    list.append(chr(97+i)*(i+1))
-print(list)
