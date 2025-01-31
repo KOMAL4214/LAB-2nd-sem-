58284318bd963dee4e21176bc43972e683f9aab9
KOMAL4214 <jksai2006@gmail.com>
2025-01-31 14:22:36 +0530

    Update ISFIBO.py

--- ISFIBO.py
+++ ISFIBO.py
@@ -2,18 +2,20 @@ t=int(input("enter the number of testcases :"))
 while(t>0) :
     n=int(input("enter the required number :"))
     l=[]
-    nxtnum=0
-    n1=0
-    n2=1
-    l.append(n1)
-    l.append(n2)
-    while(n!=nxtnum) :
+    def isfibo(n):
+        n1=0
+        n2=1
+        l.append(n1)
+        l.append(n2)
         nxtnum=n1+n2
-        l.append(nxtnum)
-        n1=n2
-        n2=nxtnum
-    if(n  in l) :
-        print("ISFIBO")
-    else :
-        print("ISNOTFIBO")
+        while(nxtnum<=n):
+            nxtnum=n1+n2
+            l.append(nxtnum)
+            n1=n2
+            n2=nxtnum
+        if(n in l):
+            print("isfibo")
+        else:
+            print("isnotfibo")
+    isfibo(n)
     t-=1
