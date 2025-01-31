b08cb0377d3356fc4d4c24161e75ac22a2f13d78
KOMAL4214 <jksai2006@gmail.com>
2025-01-31 14:21:45 +0530

    Update UTOPIAN TREE.py

--- UTOPIAN TREE.py
+++ UTOPIAN TREE.py
@@ -4,11 +4,17 @@ while(t>0) :
     i=int(input("enter the number of cycles :"))
     height=1
     k=1
+    def monsoon(height):
+        height*=2
+        return height
+    def summer(height):
+        height+=1
+        return height
     while(k<=i) :
-        if(k%2==0) :
-            height+=1
+        if(k%2!=0) :
+            height=monsoon(height)
         else :
-            height*=2
+            height=summer(height)
         k+=1
     print("the height of the sapling after ",i,"cycles is ",height,"meters")
     t-=1
