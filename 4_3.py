f4104170a0f329b1a784b683b7781135830d8bab
KOMAL4214 <jksai2006@gmail.com>
2025-02-07 18:46:07 +0530

    Update 4_3.py

--- 4_3.py
+++ 4_3.py
@@ -1,9 +1,12 @@
-alphabets=[chr(i) for i in range (97,123)]
-str=input("enter the string : ")
-str=list(str)
-for i in range(0,26):
-    if(alphabets[i] not in str):
-        print("not palagram")
-        break
-    elif(i==25):
-        print("palagram")
+t=int(input("enter the number of test cases : "))
+while(t>0):
+    alphabets=[chr(i) for i in range (97,123)]
+    str=input("enter the string : ")
+    str=list(str)
+    for i in range(0,26):
+        if(alphabets[i] not in str):
+            print("not palagram")
+            break
+        elif(i==25):
+            print("palagram")
+    t-=1
